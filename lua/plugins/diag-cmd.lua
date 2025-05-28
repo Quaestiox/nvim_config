594bade11c1c3df335ac9df08f326c0d9bb236fc
Quaestio <Quaestio@kdslife.onaliyun.com>
2025-05-28 10:33:02 +0800

    .

--- lua/plugins/diag-cmd.lua
+++ lua/plugins/diag-cmd.lua
@@ -2,12 +2,18 @@ local diag = require("diag-cmd")
 diag.setup({
 	show_config = {
 		virtual_text = false,
+        virtual_line = false,
 		signs = true,
 		underline = true,
 		update_in_insert = false,
 	},
 	disabled_file_type = { "lua", "php" },
+	float = {
+		scope = "line",
+		border = "rounded",
+		source = "always",
+		focusable = false,
+		header = "💡",
+		jump = true,
+	},
 })
-
-
-
